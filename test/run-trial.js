6018f490d3133913b1185b7bfb525ba6017b5b2d
Seth Bashford <seth.bashford@boystown.org>
2022-06-17 11:21:38 -0500

    add test

--- test/run-trial.js
+++ test/run-trial.js
@@ -1,4 +1,7 @@
 function runTrial(videoPlayer) {
+  videoPlayer.setOnFinish(() => {
+    videoPlayer.hide();
+  });
   videoPlayer.play();
 }
 
@@ -7,10 +10,20 @@ import assert from "assert";
 class VideoPlayerStub {
   constructor() {
     this.played = false;
+    this.hidden = false;
+    this.onFinish = () => {};
+  }
+
+  setOnFinish(f) {
+    this.onFinish = f;
   }
 
   play() {
     this.played = true;
+  }
+
+  hide() {
+    this.hidden = true;
   }
 }
 
@@ -19,5 +32,13 @@ describe("runTrial()", () => {
     const videoPlayer = new VideoPlayerStub();
     runTrial(videoPlayer);
     assert.equal(videoPlayer.played, true);
+  });
+
+  it("should hide the video when finished", () => {
+    const videoPlayer = new VideoPlayerStub();
+    runTrial(videoPlayer);
+    assert.equal(videoPlayer.hidden, false);
+    videoPlayer.onFinish();
+    assert.equal(videoPlayer.hidden, true);
   });
 });
