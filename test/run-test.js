56ba5ff4a91bd392af2294fb6e7848822730644f
Seth Bashford <seth.bashford@boystown.org>
2022-06-17 14:27:27 -0500

    add test

--- test/run-test.js
+++ test/run-test.js
@@ -1,7 +1,10 @@
 import assert from "assert";
 
-function runTest(startButton) {
-  startButton.setOnClick(() => startButton.hide());
+function runTest(startButton, trials) {
+  startButton.setOnClick(() => {
+    startButton.hide();
+    trials.runNext();
+  });
   startButton.show();
 }
 
@@ -25,18 +28,39 @@ class ButtonStub {
   }
 }
 
+class TrialsStub {
+  constructor() {
+    this.nextRun = false;
+  }
+
+  runNext() {
+    this.nextRun = true;
+  }
+}
+
 describe("runTest()", () => {
   it("should show start button", () => {
     const startButton = new ButtonStub();
-    runTest(startButton);
+    const trials = new TrialsStub();
+    runTest(startButton, trials);
     assert.equal(startButton.shown, true);
   });
 
   it("should hide start button when touched", () => {
     const startButton = new ButtonStub();
-    runTest(startButton);
+    const trials = new TrialsStub();
+    runTest(startButton, trials);
     assert.equal(startButton.hidden, false);
     startButton.onClick();
     assert.equal(startButton.hidden, true);
+  });
+
+  it("runs next trial when start button touched", () => {
+    const startButton = new ButtonStub();
+    const trials = new TrialsStub();
+    runTest(startButton, trials);
+    assert.equal(trials.nextRun, false);
+    startButton.onClick();
+    assert.equal(trials.nextRun, true);
   });
 });
