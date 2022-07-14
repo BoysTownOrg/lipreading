71a75bb1f79edd10cdcf142672bb857c04851001
Seth Bashford <seth.bashford@boystown.org>
2022-07-14 09:28:11 -0500

    refactor

--- test/parse-trial-urls.js
+++ test/parse-trial-urls.js
@@ -1,28 +1,5 @@
-function parseTrialUrls(text) {
-  let readyForImages = false;
-  let videoStem = "";
-  const trialUrls = [];
-  for (const line of text.split("\n"))
-    if (readyForImages) {
-      const imageStems = line.split(" ");
-      trialUrls.push({
-        image: {
-          topLeft: `${imageStems[0]}.jpg`,
-          topRight: `${imageStems[1]}.jpg`,
-          bottomLeft: `${imageStems[2]}.jpg`,
-          bottomRight: `${imageStems[3]}.jpg`,
-        },
-        video: `${videoStem}.mp4`,
-      });
-      readyForImages = false;
-    } else {
-      videoStem = line;
-      readyForImages = true;
-    }
-  return trialUrls;
-}
-
 import assert from "assert";
+import { parseTrialUrls } from "../parse-trial-urls.js";
 
 function assertEqualTrialUrl(actual, expected) {
   assert.equal(actual.image.topLeft, expected.image.topLeft);
